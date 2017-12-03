600898124b5355280edc2cc471c486c4a19d49ab
Edgar Nightingale <edgar.nightingale@btinternet.com>
2017-12-03 14:09:46 +0000

    feat: Added markers for nearby restaurants to the map

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-let map;
-
 function initMap() {
 	// Styles array for custom map
 	const styles = [
@@ -141,18 +139,75 @@ function initMap() {
     }];
 
 	// Constructor creates a new map
-	map = new google.maps.Map(document.getElementById('map'), {
+	const map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 51.96403089999999, lng: 1.3512195}, 
 		zoom: 14,
 		styles: styles,
 		mapTypeControl: false
 	});
+	
+	// Array to store all the place markers
+	let markers = [];
+	
+	// Search request for restaurants within 2000 metres of Felixstowe centre
+	function getMarkers() {
+		const felixstowe = new google.maps.LatLng({lat: 51.961726, lng: 1.351255});
+		const request = {
+					location: felixstowe,
+					radius: '2000',
+					type: ['restaurant'],
+					key: 'AIzaSyChGiQPjP2sWVf0rZpvoNKarXBNI5VzRpA'
+					};
+		
+		service = new google.maps.places.PlacesService(map);
+		service.nearbySearch(request, createMarkers);
+	}
+	
+	getMarkers();
+	
+	// Creates markers from results of the nearbysearch
+	function createMarkers(results, status, pagination) {
+		if (status === google.maps.places.PlacesServiceStatus.OK) {
+			
+			// Increases the results to a maximum of 60
+			/*if (pagination.hasNextPage) {
+				pagination.nextPage();
+			}*/
+			
+			// Loop through results and create marker for each location
+			for (let i = 0; i < results.length; i++) {
+				let position = results[i].geometry.location;
+				let title = results[i].name;
+				
+				let marker = new google.maps.Marker({
+					position: position,
+					map: map,
+					title: title,
+					animation: google.maps.Animation.DROP,
+					id: i
+				})
+				
+				// Push the marker to our array of markers.
+				markers.push(marker);
+
+				// Create an onclick event to open the large infowindow at each marker.
+				// marker.addListener('click', function() {
+				// populateInfoWindow(this, largeInfowindow);
+				// });
+			}
+		}
+	}
 }
 
+
+// ViewModel
 const ViewModel = function() {
 	const self = this;
+	
+	// Menu default state is visible
 	this.menuHidden = ko.observable(false);
 	
+	// Toggle menu visibility when clicking the icon
 	this.toggleMenu = function() {
 		if (self.menuHidden() === true) {
 			self.menuHidden(false);
